36c1e3bdfcf44767140fe82cbb9e8ecde0e5f314
unknown <zchenyang@AREA51-PC.research.eecs.qmul.ac.uk>
2015-08-04 12:42:52 +0100

    updating learning result

--- stateReward.m
+++ stateReward.m
@@ -5,6 +5,6 @@ function r = stateReward(world, state)
 %% 
     pos_error = state(1) - world.desPos;
     vel_error = state(2) - 0;
-    r =  50 - pos_error'*world.Q*pos_error - vel_error'*world.R*vel_error;
-    r = max(r,0);
+    r = - pos_error'*world.Q*pos_error - vel_error'*world.R*vel_error;
+    r = max(r,-50);
 end
